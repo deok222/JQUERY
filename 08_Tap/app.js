8aeaa78c000861460de31cde7140e5d81e9fe748
deok222 <crarus56@gmail.com>
2022-02-25 10:32:57 +0900

    tap

---
+++ 08_Tap/app.js
@@ -0,0 +1,18 @@
+// $(function () {
+//   var btns = $(".tab li");
+//   var content = $(".content");
+
+//   btns.eq(0).find("a").addClass("active");
+//   content.hide().eq(0).show();
+// });
+
+// var btns = $(".tab li");
+// var content = $(".content");
+
+// btns.eq(0).find("a").addClass("active");
+// content.hide().eq(0).show();
+
+// btns.click(function () {
+//   btns.find("a").removeClass("active");
+//   $(this).find("a").addClass("active");
+// });
